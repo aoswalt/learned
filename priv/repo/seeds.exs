f8fa087e9b0601821101f4459bcb6b8acc551f18
Adam Oswalt <email@adamoswalt.com>
2017-09-02 21:39:35 -0500

    add some styling to til listing

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -27,6 +27,9 @@ til_list = [
   "another one",
   "random message",
   "lorem ipsum dolor sit amet",
+  "Et minus necessitatibus et quia accusantium tenetur esse. Temporibus et totam aspernatur est beatae adipisci corporis. Velit omnis aut magnam. Corrupti iure dolore architecto qui officia optio odio.",
+  "Molestiae pariatur vel et aliquam omnis qui optio exercitationem. Aliquid non molestias quaerat esse magni sit. Nihil architecto esse repellat atque tenetur reprehenderit. Perferendis voluptatem qui voluptatem id id nostrum aperiam.",
+  "Atque aut ipsam repudiandae adipisci. Aliquid harum nesciunt temporibus non. Dolor fuga tempore debitis sed temporibus molestiae minima. Qui dicta quo suscipit consectetur vel esse est.\nUllam doloremque fugiat est nemo. Magnam saepe sit minima id. Itaque est illum quo. Nemo mollitia quos excepturi dolores.",
 ]
 
 alias Learned.Til
@@ -38,4 +41,7 @@ Repo.insert!(%Til{text: Enum.at(til_list, 1), user_id: 3})
 Repo.insert!(%Til{text: Enum.at(til_list, 2), user_id: 2})
 Repo.insert!(%Til{text: Enum.at(til_list, 3), user_id: 4})
 Repo.insert!(%Til{text: Enum.at(til_list, 4), user_id: 1})
-Repo.insert!(%Til{text: Enum.at(til_list, 4), user_id: 3})
+Repo.insert!(%Til{text: Enum.at(til_list, 5), user_id: 3})
+Repo.insert!(%Til{text: Enum.at(til_list, 6), user_id: 3})
+Repo.insert!(%Til{text: Enum.at(til_list, 7), user_id: 2})
+Repo.insert!(%Til{text: Enum.at(til_list, 8), user_id: 1})

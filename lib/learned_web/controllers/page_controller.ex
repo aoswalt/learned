81ea647057524a7f45f55fe38e746c31059e401b
Adam Oswalt <email@adamoswalt.com>
2017-09-02 21:12:52 -0500

    show user name with tils

--- lib/learned_web/controllers/page_controller.ex
+++ lib/learned_web/controllers/page_controller.ex
@@ -2,9 +2,10 @@ defmodule LearnedWeb.PageController do
   use LearnedWeb, :controller
   alias Learned.Repo
   alias Learned.Til
+  import Ecto.Query, only: [from: 2]
 
   def index(conn, _params) do
-    tils = Repo.all(Til)
+    tils = Repo.all(from t in Til, preload: [:user])
     render conn, "index.html", tils: tils
   end
 end
--- lib/learned_web/views/page_view.ex
+++ lib/learned_web/views/page_view.ex
@@ -1,7 +1,9 @@
 defmodule LearnedWeb.PageView do
   use LearnedWeb, :view
   alias Learned.Til
+  alias Learned.User
 
   def text(%Til{text: text}), do: text
   def user_id(%Til{user_id: user_id}), do: user_id
+  def user_name(%Til{user: %User{name: name}}), do: name
 end

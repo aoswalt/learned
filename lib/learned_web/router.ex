f4907d8d6e181c52800a84721ae913e873fd8d51
Adam Oswalt <email@adamoswalt.com>
2017-09-09 16:07:47 -0500

    common pipeline for browser and api to share routing

--- lib/learned_web/router.ex
+++ lib/learned_web/router.ex
@@ -1,30 +1,17 @@
 defmodule LearnedWeb.Router do
   use LearnedWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
+  pipeline :common do
+    plug :accepts, ["json", "html"]
+    # TODO(adam): make plug that skips based on accepts?
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", LearnedWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", TilController, :list
-    get "/tils", TilController, :list
-    get "/tils/:id", TilController, :show
-    get "/users/:user_id", UserController, :show
-  end
-
-  # Other scopes may use custom stacks.
-  scope "/api", LearnedWeb do
-    pipe_through :api
+    pipe_through :common # Use shared browser api pipeline
 
     get "/", TilController, :list
     get "/tils", TilController, :list

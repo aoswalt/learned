35057c49644cab4f3ed90754e5b3570368eeb344
Adam Oswalt <email@adamoswalt.com>
2017-09-24 19:06:23 -0500

    conditional browers plugs rough but works

--- lib/learned_web/router.ex
+++ lib/learned_web/router.ex
@@ -1,13 +1,23 @@
 defmodule LearnedWeb.Router do
   use LearnedWeb, :router
+  alias Plug.Conn
+
+  defp conditional_plugs(%Conn{private: %{phoenix_format: "html"}} = conn, opts) do
+    # TODO(adam): is there a way to use the plug macro here?
+    conn
+    |> fetch_session(opts)
+    |> fetch_flash(opts)
+    |> protect_from_forgery(opts)
+    |> put_secure_browser_headers(opts)
+  end
+
+  defp conditional_plugs(conn, _opts) do
+    conn
+  end
 
   pipeline :common do
     plug :accepts, ["json", "html"]
-    # TODO(adam): make plug that skips based on accepts?
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :conditional_plugs
   end
 
   scope "/", LearnedWeb do

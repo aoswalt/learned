2109c75ac26e1fc47381ad1f677dedd23065e6f7
Adam Oswalt <email@adamoswalt.com>
2017-09-28 18:40:38 -0500

    add comment about 'mix ecto.reset'

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,6 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+#     mix ecto.reset will rebuild the database with seeding
 
 alias Learned.Repo
 
